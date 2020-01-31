71a0af3ee76701539c7cd28dcdabb6675caa5fcd
Daniel Hiller <daniel.hiller.1972@gmail.com>
2020-01-31 16:08:02 +0100

    Updated demo script with figlet demo name display
    Signed-off-by: Daniel Hiller <daniel.hiller.1972@gmail.com>

--- show-demos.sh
+++ show-demos.sh
@@ -6,6 +6,9 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+( which figlet ) > /dev/null 2>&1
+[ $? -eq 0 ] && figlet_available=1 || figlet_available=0
+
 set -euo pipefail
 
 if [ "$#" -gt 0 ]; then
@@ -37,13 +40,21 @@ function stop_it {
 trap stop_it SIGINT SIGTERM
 
 while true; do
+    clear
     for demo_file in $(ls *-ascii.cast); do
         if [ "$start_at" -gt 0 ]; then
             start_at=$(($start_at-1))
             continue
         fi
-        echo "Next demo: $demo_file..."
-        echo
+        demo_name=$(echo $demo_file | sed -E 's/[0-9]+-kv-(.*)-ascii.cast/\1/')
+        if [ $figlet_available ]; then
+            width=$(tput cols)
+            figlet -w "$width" "$demo_name"
+            echo
+        else
+            echo "Next demo: $demo_name..."
+            echo
+        fi
         sleep 1
         asciinema play -i 1 -s 1.5 "$demo_file"
         [ -z "$must_stop" ] || exit 0
